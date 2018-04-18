dda4a28aa05a19ec3bfc0cf3b1a32b480387bcfa
Manuel Cuesta <mecuesta@udel.edu>
2018-04-18 14:33:04 -0400

    saving work to crab, crawler, and interObj to switch branches, almost ready to use in Model(Game) and View

--- Crawler.java
+++ Crawler.java
@@ -1,2 +1,56 @@
-class Crawler{
+abstract class Crawler{
+	int xloc;
+	int yloc;
+	int xIncr;
+	int yIncr;
+
+	public void setX(int newX) {
+		xloc = newX;
+	}
+	public void setYLoc(int newY) {
+		yloc = newY;
+	}
+	public int getXLoc() {
+		return xloc;
+	}
+	public int getYLoc() {
+		return yloc;
+	}
+	// move() will be implemented differently for InterObj and Crab
+	public void move() {	}
+
+
+
+
+
+	public void onCollision(Crab crab, InterObj interObj) {
+		if (interObj.getName() == "trash") {
+			crab.disappear();
+			crab.removeLife();
+			interObj.disappear();
+			//Code to make the crab disappear
+			//Code to remove one life from crab
+			//Code to make trash disappear
+		}
+		else if (interObj.getName() == "invasive") {
+			crab.addScore();
+			interObj.disappear();
+			//Code to add score to crab
+			//Code to make invasive disappear
+		}
+		else if (interObj.getName() == "power up") {
+			interObj.disappear();
+			crab.powerUp(true);
+			//Code to make power up disappear
+			//Code to change the abilities of the crab
+			//code to handle the shift to power up and back from power up
+		}
+		else if (interObj.getName() == "native") {
+			crab.subScore();
+			interObj.disappear();
+			//Code to remove score from crab
+			//Code to make native disappear
+		}
+	}
+
 }
--- InterObj.java
+++ InterObj.java
@@ -1,2 +1,25 @@
-class InterObj{
+abstract class InterObj extends Crawler {
+	
+	private String name;
+
+
+	InterObj(String objName) {
+		this.name = objName;
+		//randomize the yIncr and xIncr and then also location of spawn
+		//coming out of the right side screen
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void disappear() {
+		//I think this is the same code that should go in either Crawler or the View
+		//for both InterObj and Crab
+	}
+
+
+	
+
+
 }
