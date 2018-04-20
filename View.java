74a4f5ee2e2300972677ac9673e651e7a8cc71a8
Matt Grossman <mattig@udel.edu>
2018-04-20 15:44:25 -0400

    Catching up with my branch

--- View.java
+++ View.java
@@ -22,8 +22,8 @@ public class View extends JFrame{
    	final static int frameHeight = 800;//300
     final static int imgWidth = 165;//165
 	final static int imgHeight = 165;
-    
-    public View(ArrayList<InterObj> s){    
+
+    public View(ArrayList<InterObj> s){
         drawPanel = new DrawPanel();
         score = 0;
         lives = 3;
@@ -63,7 +63,7 @@ public class View extends JFrame{
             drawCrab();
             drawInterObjs();
 		}
-        
+
         @Override
 		public Dimension getPreferredSize() {
 			return new Dimension(frameStartSize, frameStartSize);
@@ -79,9 +79,9 @@ public class View extends JFrame{
     }
 
     private BufferedImage createImage(String fname){
-    		
+
 		BufferedImage bufferedImage;
-    		
+
     		try {
     			// Method now takes a String (fname) in order to get the 
 			// right image to load for all the images passed through
