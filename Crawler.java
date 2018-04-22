73cc4000a6530ddc487abcd0a981de913e74d789
Manuel Cuesta <mecuesta@udel.edu>
2018-04-22 19:26:28 -0400

    Added setters for xIncr and yIncr

--- Crawler.java
+++ Crawler.java
@@ -1,8 +1,8 @@
 abstract class Crawler{
-	int xloc;
-	int yloc;
-	int xIncr;
-	int yIncr;
+	private int xloc;
+	private int yloc;
+	private int xIncr;
+	private int yIncr;
 
 	public void setXLoc(int newX) {
 		xloc = newX;
@@ -15,6 +15,12 @@ abstract class Crawler{
 	}
 	public int getYLoc() {
 		return yloc;
+	}
+	public void setYIncr(int newYIncr) {
+		yIncr = newYIncr;
+	}
+	public void setXIncr(int newXIncr) {
+		xIncr = newXIncr;
 	}
 	// move() will be implemented differently for InterObj and Crab
 	public abstract void move();
