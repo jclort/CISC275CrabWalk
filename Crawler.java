759296e9097d61b187625ea19ed0517b59baaf7e
Jeffrey Lort <jclort@udel.edu>
2018-04-22 22:45:31 -0400

    fixed some weird git stuff that was in Crawler

--- Crawler.java
+++ Crawler.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-class Crawler{ // This is our movable dude
-=======
+
 abstract class Crawler{
 	private int xloc;
 	private int yloc;
@@ -27,43 +25,4 @@ abstract class Crawler{
 	}
 	// move() will be implemented differently for InterObj and Crab
 	public abstract void move();
-
-
-<<<<<<< HEAD
-
-
-
-//	public void onCollision(Crab crab, InterObj interObj) {
-//		if (interObj.getName() == "trash") {
-//			crab.disappear();
-//			crab.removeLife();
-//			//interObj.disappear();
-//			//Code to make the crab disappear
-//			//Code to remove one life from crab
-//			//Code to make trash disappear
-//		}
-//		else if (interObj.getName() == "invasive") {
-//			crab.addScore();
-//			//interObj.disappear();
-//			//Code to add score to crab
-//			//Code to make invasive disappear
-//		}
-//		else if (interObj.getName() == "power up") {
-//			//interObj.disappear();
-//			crab.powerUp(true);
-//			//Code to make power up disappear
-//			//Code to change the abilities of the crab
-//			//code to handle the shift to power up and back from power up
-//		}
-//		else if (interObj.getName() == "native") {
-//			crab.subScore();
-//			//interObj.disappear();
-//			//Code to remove score from crab
-//			//Code to make native disappear
-//		}
-//	}
-//
->>>>>>> 2ce471109ff2313b0a42d57c6dc8f792c5d373c6
-=======
->>>>>>> b767b367e0739e28edf13b1b113b1d4b106afef2
 }
