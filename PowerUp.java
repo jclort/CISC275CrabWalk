35b7fbcdcce282bde1a64ba4a1620a4e2d12f703
Jeffrey Lort <jclort@udel.edu>
2018-05-04 13:56:33 -0400

    added equals methods to the framework classes, and wrote tests for the model class

--- PowerUp.java
+++ PowerUp.java
@@ -1,9 +1,21 @@
+import java.util.Random;
+
 public class PowerUp extends InterObj {
 	Random rand = new Random();
 	private int max = 3;
 	final private int GLOBAL = 6;
 
-
+	@Override
+	public boolean equals(Object other){
+		if (!(other instanceof PowerUp)){
+			return false;
+		}
+		else{
+			PowerUp o = (PowerUp)other;
+			return super.equals(o);
+		}
+	}
+	
 	public void onCollision(Crab crab) {
 		this.collisionBool = true;
 	}
