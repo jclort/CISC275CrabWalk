6ca39e74b0698aff5a92d124f59334d0d1a1a680
Matt Grossman <mattig@udel.edu>
2018-05-06 16:30:59 -0400

    Catching up

--- Crab.java
+++ Crab.java
@@ -1,10 +1,14 @@
-class Crab extends Crawler {
+public class Crab extends Crawler {
     	private Direction dir;
 	private boolean powerUp = false;
 	private int score = 0;
 	private int lives = 4;
     	private int boundary;
-	
+    /**
+     * Constructor for the Crab
+     * @param frameSize The size of the frame the crab will be in
+     * @return Nothing. It's just the constructor
+     */
 	public Crab(int frameSize) {
 		xIncr = 0;
 		yIncr = 0;
@@ -13,26 +17,39 @@ class Crab extends Crawler {
 		this.setXLoc(boundary);
         	dir = Direction.STILL;
 	}
-
+    /**
+     * This will be responsible for moving the crab across the screen
+     *@param None
+     *@return None
+     */
 	public void move() {
         	if (xloc <= boundary-xIncr){
 			yloc += yIncr;
 			xloc += xIncr;
         	}
 	}
-
+    /**
+       Meant for adding to the score of the game
+     */
 	public void addScore() {
 		score += 2;
 	}
-
+    /**
+       Subtracting from the score of the game
+    */
 	public void subScore() {
 		score -= 1;
 	}
-
+    /**
+       Takes a life away if your health runs out
+     */
 	public void removeLife() {
 		lives -= 1;
 	}
-
+    /**
+       Gets the total score of the game
+       @return  The total score of the game
+    */
 	public int getTotalScore() {
 		return score+(lives*5);
 	}
